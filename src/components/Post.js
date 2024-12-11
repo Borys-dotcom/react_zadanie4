9acfd5032322187c59348de24f936cf1cf912a9c
Borys-dotcom <184245222+Borys-dotcom@users.noreply.github.com>
2024-12-11 13:09:13 +0100

    likes handling added

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,54 @@
 import './Post.css'
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import axios from 'axios';
 
 const Post = (props) => {
 
     const [likesCount, setLikesCount] = useState(props.post.likes.length);
+    const [displayModal, setDisplayModal] = useState(false);
+    const [isPostLikedByUser, setIsPostLikedByUser] = useState(false);
+
+    useEffect(() => {
+        props.post.likes.forEach((like) => {
+            if (like.username === props.user) {
+                setIsPostLikedByUser(true);
+            }
+        })
+    }, []);
+
+    const postLike = () => {
+
+        axios.post('https://akademia108.pl/api/social-app/post/like', {
+            "post_id": props.post.id
+        })
+            .then(() => {
+                setLikesCount((prevLikesCount) => {
+                    return prevLikesCount + 1
+                });
+                setIsPostLikedByUser(true);
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+
+    }
+
+    const postDislike = () => {
+
+        axios.post('https://akademia108.pl/api/social-app/post/dislike', {
+            "post_id": props.post.id
+        })
+            .then((res) => {
+                setLikesCount((prevLikesCount) => {
+                    return prevLikesCount - 1
+                });
+                setIsPostLikedByUser(false);
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+
+    }
 
     return (
         <div className='post'>
@@ -16,12 +61,27 @@ const Post = (props) => {
                     <div className="data">{props.post.created_at.substring(0, 10)}</div>
                 </div>
                 <div className="postContent">{props.post.content}</div>
-                <div className="likes">{likesCount}</div>
+                <div className='likeDislike'>
+                    <div className='likeDislikeBtnContainer'>
+                        {!isPostLikedByUser && props.user &&
+                            <button className='btn like' onClick={postLike}>Like</button>}
+                        {isPostLikedByUser && props.user &&
+                            <button className='btn dislike' onClick={postDislike}>Dislike</button>}
+                    </div>
+                    <div className="likes">{likesCount}</div>
+                </div>
                 <div className='btnContainer'>
                     {(props.user === props.post.user.username) &&
-                        <button className='btn' onClick={()=>props.deletePost(props.post.id)}>usuń post</button>}
+                        <button className='btn' onClick={() => setDisplayModal(true)}>usuń post</button>}
                 </div>
             </div>
+            {displayModal &&
+                <div className='modalMessage'>
+                    <h3>Czy chcesz usunąć post?</h3>
+                    <button className='btn yes' onClick={() => props.deletePost(props.post.id)}>Tak</button>
+                    <button className='btn no' onClick={() => setDisplayModal(false)}>Nie</button>
+                </div>
+            }
         </div>
     )
 }
